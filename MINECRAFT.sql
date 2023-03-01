050f6c7342a5d0c50d122b68157141dcef40bc2f
proaro2001 <proaro2001@gmail.com>
2023-03-01 13:48:17 -0800

    update updates

--- MINECRAFT.sql
+++ MINECRAFT.sql
@@ -1,25 +1,26 @@
--- CREATE DATABASE `MINECRAFT`;
--- DROP DATABASE `MINECRAFT`;
--- SHOW DATABASES;
--- USE `MINECRAFT`;
+SHOW DATABASES;
+CREATE DATABASE `MINECRAFT`;
+SHOW DATABASES;
 
-DROP TABLE ARMOR;
-DROP TABLE MOB;
+USE `MINECRAFT`;
+
+-- DROP TABLE ARMOR;
+-- DROP TABLE MOB;
 
 -- Ka Hin 
 -- Note: It is just initalizing my part
 CREATE TABLE `ARMOR`(
 	`NamingID` INT PRIMARY KEY UNIQUE NOT NULL ,
-    `Name` VARCHAR(41) UNIQUE NOT NULL,
-    -- the longest item name has 41 characters inlcuding spaces
+    `Name` VARCHAR(41) UNIQUE NOT NULL, 
 	`Material` VARCHAR(41) NOT NULL,
     `Durability` INT NOT NULL,
-    `knockback reduction` INT ,
+    `knockback reduction` INT,
     `Armor toughness` INT,
-    `Armor Resistance` INT
+    `Armor Resistance` INT,
+    `Enchantment ID` INT NOT NULL
 );
 
--- DESCRIBE `ARMOR`; -- display how we store the ARMOR TABLE
+DESCRIBE `ARMOR`; -- display how we store the ARMOR TABLE
 
 CREATE TABLE MOB(
 `ID` INT PRIMARY KEY UNIQUE NOT NULL,
@@ -28,12 +29,27 @@ CREATE TABLE MOB(
 `Damage Per Hit` INT,
 `Attack Speed` INT,
 `Special Abilities` VARCHAR(50), -- temporary set to 50 words
-`Total Resistance` INT
+`Total Resistance` INT,
+`ARMOR ID` INT  UNIQUE NOT NULL,
+`WEAPON ID` INT UNIQUE NOT NULL
+-- ARMOR ID and WEAPON are foreign key
 );
 
--- DESCRIBE `MOB`; -- display how we store the MOB TABLE
+DESCRIBE `MOB`; -- display how we store the MOB TABLE
 
--- CREATE TABLE AFFECT(
+CREATE TABLE `POTION`(
+    `Potion ID` INT PRIMARY KEY UNIQUE NOT NULL,
+    `Name` VARCHAR( 20 ) UNIQUE NOT NULL
+);
 
--- );
+DESCRIBE `POTION`;
+
+CREATE TABLE `POTION AFFECT`(
+    `Potion ID` INT ,
+    `Mob ID` INT NOT NULL
+);
+
+DESCRIBE `POTION AFFECT`;
 -- end of Ka Hin 
+
+DROP DATABASE `MINECRAFT`;
