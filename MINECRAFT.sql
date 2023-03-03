4d8df7afd3b0f2ec7e925ca418803f3b4154dc68
proaro2001 <proaro2001@gmail.com>
2023-03-03 05:02:23 +0000

    Error: Table WEAPON, MATERIAL, RECIPE

--- MINECRAFT.sql
+++ MINECRAFT.sql
@@ -10,14 +10,13 @@ USE `MINECRAFT`;
 -- Ka Hin 
 -- Note: It is just initalizing my part
 CREATE TABLE `ARMOR`(
-	`NamingID` INT PRIMARY KEY UNIQUE NOT NULL ,
-    `Name` VARCHAR(41) UNIQUE NOT NULL, 
-	`Material` VARCHAR(41) NOT NULL,
-    `Durability` INT NOT NULL,
-    `knockback reduction` INT,
-    `Armor toughness` INT,
-    `Armor Resistance` INT,
-    `Enchantment ID` INT NOT NULL
+   `NamingID` VARCHAR(41) PRIMARY KEY UNIQUE NOT NULL ,
+   `Name` VARCHAR(41) UNIQUE NOT NULL,
+   `Material` VARCHAR(41) NOT NULL,
+   `Durability` INT NOT NULL,
+   `Defense Points` FLOAT,
+   `Knockback Resistance` FLOAT,
+   `Enchantment ID` INT
 );
 
 -- Helmets
@@ -91,44 +90,44 @@ CREATE TABLE `POTION AFFECT`(
 DESCRIBE `POTION AFFECT`;
 -- end of Ka Hin 
 
---------------------------------malachi 3/1------------------------------------
+-- --------------------------------malachi 3/1------------------------------------
 
-CREATE TABLE `WEAPON`(
-    `WeaponID` INT PRIMARY KEY UNIQUE NOT NULL ,
-    `Name` VARCHAR( 'Wooden Shovel', 'Stone Shovel', 'Iron Shovel', 'Gold Shovel', 'Diamond Shovel', 'Netherite Shovel'
-                    'Wooden Hoe', 'Stone Hoe', 'Iron Hoe', 'Gold Hoe', 'Diamond Hoe', 'Netherite Hoe'
-                    'Wooden Sword', 'Stone Sword', 'Iron Sword', 'Gold Sword', 'Diamond Sword', 'Netherite Sword'
-                    'Wooden Axe', 'Stone Axe', 'Iron Axe', 'Gold Axe', 'Diamond Axe', 'Netherite Axe'
-                    'Wooden Pickaxe', 'Stone Pickaxe', 'Iron Pickaxe', 'Gold Pickaxe', 'Diamond Pickaxe', 'Netherite Pickaxe'
-                    ) UNIQUE NOT NULL,
-	`MaterialType` VARCHAR('Wooden', 'Stone', 'Iron', 'Gold', 'Diamond', 'Netherite') NOT NULL,
-    `WeaponType` VARCHAR('Shovel', 'Hoe', 'Sword', 'Axe', 'Pickaxe') NOT NULL,
-    `WeaponRecipe` RECIPE
-    `durability` INT  NOT NULL,
-    `knockback` INT NOT NULL,
-    `base damage` INT NOT NULL,
-    `reload time` INT NOT NULL,
-);
+-- CREATE TABLE `WEAPON`(
+--     `WeaponID` INT PRIMARY KEY UNIQUE NOT NULL ,
+--     `Name` VARCHAR( 'Wooden Shovel', 'Stone Shovel', 'Iron Shovel', 'Gold Shovel', 'Diamond Shovel', 'Netherite Shovel'
+--                     'Wooden Hoe', 'Stone Hoe', 'Iron Hoe', 'Gold Hoe', 'Diamond Hoe', 'Netherite Hoe'
+--                     'Wooden Sword', 'Stone Sword', 'Iron Sword', 'Gold Sword', 'Diamond Sword', 'Netherite Sword'
+--                     'Wooden Axe', 'Stone Axe', 'Iron Axe', 'Gold Axe', 'Diamond Axe', 'Netherite Axe'
+--                     'Wooden Pickaxe', 'Stone Pickaxe', 'Iron Pickaxe', 'Gold Pickaxe', 'Diamond Pickaxe', 'Netherite Pickaxe'
+--                     ) UNIQUE NOT NULL,
+-- 	`MaterialType` VARCHAR('Wooden', 'Stone', 'Iron', 'Gold', 'Diamond', 'Netherite') NOT NULL,
+--     `WeaponType` VARCHAR('Shovel', 'Hoe', 'Sword', 'Axe', 'Pickaxe') NOT NULL,
+--     `WeaponRecipe` RECIPE
+--     `durability` INT  NOT NULL,
+--     `knockback` INT NOT NULL,
+--     `base damage` INT NOT NULL,
+--     `reload time` INT NOT NULL,
+-- );
 
-CREATE TABLE `Material`(
-    `MaterialID` INT PRIMARY KEY NOT NULL,
-    `Name` VARCHAR(`*EMPTY*`, `Stick`, `Oak Plank`, `Cobblestone`, `Iron Ingot`, `Gold Ingot`, `Diamond`, `Netherite Ingot`, `String`)
-);
+-- CREATE TABLE `Material`(
+--     `MaterialID` INT PRIMARY KEY NOT NULL,
+--     `Name` VARCHAR(`*EMPTY*`, `Stick`, `Oak Plank`, `Cobblestone`, `Iron Ingot`, `Gold Ingot`, `Diamond`, `Netherite Ingot`, `String`)
+-- );
 
-CREATE TABLE `Recipe`(
-    `RecipeID` INT PRIMARY KEY UNIQUE NOT NULL --takes in reference key from `WEAPON` how?
-    `Material1` VARCHAR(Material.MaterialID)    -- is set to be a materialID 
-    `Material2` VARCHAR(Material.MaterialID)
-    `Material3` VARCHAR(Material.MaterialID)
-    `Material4` VARCHAR(Material.MaterialID)
-    `Material5` VARCHAR(Material.MaterialID)
-    `Material6` VARCHAR(Material.MaterialID)
-    `Material7` VARCHAR(Material.MaterialID)
-    `Material8` VARCHAR(Material.MaterialID)
-    `Material9` VARCHAR(Material.MaterialID)
-);
+-- CREATE TABLE `Recipe`(
+--     `RecipeID` INT PRIMARY KEY UNIQUE NOT NULL --takes in reference key from `WEAPON` how?
+--     `Material1` VARCHAR(Material.MaterialID)    -- is set to be a materialID 
+--     `Material2` VARCHAR(Material.MaterialID)
+--     `Material3` VARCHAR(Material.MaterialID)
+--     `Material4` VARCHAR(Material.MaterialID)
+--     `Material5` VARCHAR(Material.MaterialID)
+--     `Material6` VARCHAR(Material.MaterialID)
+--     `Material7` VARCHAR(Material.MaterialID)
+--     `Material8` VARCHAR(Material.MaterialID)
+--     `Material9` VARCHAR(Material.MaterialID)
+-- );
 
--------------------------------- end of malachi ------------------------------------
+-- -------------------------------- end of malachi ------------------------------------
 -- Kris
 CREATE TABLE ENCHANTMENT (
     ENCHANTMENT_ID int UNIQUE PRIMARY KEY,
