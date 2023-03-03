b73f64cbbb0010018a247eb7be4a39e292ad3ef4
proaro2001 <proaro2001@gmail.com>
2023-03-03 04:53:30 +0000

    update

--- MINECRAFT.sql
+++ MINECRAFT.sql
@@ -20,6 +20,45 @@ CREATE TABLE `ARMOR`(
     `Enchantment ID` INT NOT NULL
 );
 
+-- Helmets
+INSERT INTO ARMOR VALUES( 'leather_helmet', 'Leather Cap', 'Leather', 55, 1, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'golden_helmet', 'Golden Helmet', 'Gold', 77, 2, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'chainmail_helmet', 'Chainmail Helmet', 'Chainmail', 165, 2, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'iron_helmet', 'Iron Helmet', 'Iron', 165, 2, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'diamond_helmet', 'Diamond Helmet', 'Diamond', 363, 3, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'netherite_helmet', 'Netherite Helmet', 'Netherite Ingot', 407, 3, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'turtle_helmet', 'Turtle Shell', 'Scute', 275, 2, 0.1,NULL);
+
+
+-- Chestplate
+INSERT INTO ARMOR VALUES( 'leather_chestplate', 'Leather Tunic', 'Leather', 80, 3, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'golden_chestplate', 'Golden Chestplate', 'Gold', 112, 5, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'chainmail_chestplate', 'Chainmail Chestplate', 'Chainmail', 240, 5, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'iron_chestplate', 'Iron Chestplate', 'Iron', 240, 6, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'diamond_chestplate', 'Diamond Chestplate', 'Diamond', 528, 8, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'netherite_chestplate', 'Netherite Chestplate', 'Netherite Ingot', 592, 8, 0.1,NULL);
+
+
+-- leggings
+INSERT INTO ARMOR VALUES( 'leather_boots', 'Leather Pants', 'Leather', 75, 2, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'golden_boots', 'Golden Leggings', 'Gold', 105, 1.5, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'chainmail_boots', 'Chainmail Leggings', 'Chainmail', 225, 4, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'iron_boots', 'Iron Leggings', 'Iron', 225, 5, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'diamond_boots', 'Diamond Leggings', 'Diamond', 495, 6, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'netherite_boots', 'Netherite Leggings', 'Netherite Ingot', 555, 6, 0.1,NULL);
+
+
+-- boots
+INSERT INTO ARMOR VALUES( 'leather_leggings', 'Leather Boots', 'Leather', 65, 1, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'golden_leggings', 'Golden Boots', 'Gold', 91, 1, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'chainmail_leggings', 'Chainmail Boots', 'Chainmail', 195, 1, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'iron_leggings', 'Iron Boots', 'Iron', 195, 2, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'diamond_leggings', 'Diamond Boots', 'Diamond', 429,3, 0.1,NULL);
+INSERT INTO ARMOR VALUES( 'netherite_leggings', 'Netherite Boots', 'Netherite Ingot', 481, 3, 0.1,NULL);
+
+
+SELECT * FROM ARMOR;
+
 DESCRIBE `ARMOR`; -- display how we store the ARMOR TABLE
 
 CREATE TABLE MOB(
@@ -89,7 +128,43 @@ CREATE TABLE `Recipe`(
     `Material9` VARCHAR(Material.MaterialID)
 );
 
+-------------------------------- end of malachi ------------------------------------
+-- Kris
+CREATE TABLE ENCHANTMENT (
+    ENCHANTMENT_ID int UNIQUE PRIMARY KEY,
+    ENCHANTMENT_TYPE varchar(6) CHECK (ENCHANTMENT_TYPE = 'Armor' OR                                                                                                        
+    ENCHANTMENT_TYPE = 'Weapon'),
+    REGULAR_PROT int CHECK (REGULAR_PROT <= 4 AND REGULAR_PROT >= 0),
+    BLAST_PROT int CHECK (BLAST_PROT <= 4 AND BLAST_PROT >= 0),
+    KNOCKBACK_INC int CHECK (KNOCKBACK_INC <= 4 AND KNOCKBACK_INC >= 0),
+    projProt int CHECK (projProt <= 4 AND projProt >= 0),
+    fireProt int CHECK (fireProt <= 4 AND fireProt >= 0),
+    feather_fall int CHECK (feather_fall <= 4 AND feather_fall >= 0),
+    SHARPNESS int CHECK (SHARPNESS <= 5 AND SHARPNESS >= 0),
+    fireAsp int CHECK (fireAsp <= 2 AND fireAsp >= 0),
+    SMITE int CHECK (SMITE <= 4 AND SMITE >= 0),
+    baneOfA int CHECK (baneOfA <= 5 AND baneOfA >= 0),
+    KNOCKBACK int CHECK (KNOCKBACK <= 4 AND KNOCKBACK >= 0),
+    SWEEP_EDGE int CHECK (SWEEP_EDGE <= 4 AND SWEEP_EDGE >= 0),
+    POWER_ench int CHECK (POWER_ench <= 5 AND POWER_ench >= 0),
+    MULTISHOOT bool,
+    PIERCING int CHECK (PIERCING <= 4 AND PIERCING >= 0),
+    PUNCH int CHECK (PUNCH <= 2 AND PUNCH >= 0),
+    QUICK_CHARGE int CHECK (QUICK_CHARGE <= 3 AND QUICK_CHARGE >= 0),
+    FLAME bool,
+    IMAPLING bool,
+    RIPTIDE int CHECK (RIPTIDE <= 3 AND RIPTIDE >= 0)
+);
 
 
+DESCRIBE ENCHANTMENT;
 
+
+INSERT into ENCHANTMENT values (1,"Armor",1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+-------------------------------- end of Kris ----------------------------------
 DROP DATABASE `MINECRAFT`;
+
+
+
+
