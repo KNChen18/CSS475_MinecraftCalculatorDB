a8353df01578f08af562c4001d39419ef76d7d8a
proaro2001 <proaro2001@gmail.com>
2023-03-16 08:13:05 +0000

    Co-authored-by: malachino <malachino@users.noreply.github.com>

--- Sources/Queries.sql
+++ Sources/Queries.sql
@@ -48,6 +48,66 @@ FROM `ARMOR`
 WHERE `NamingID` = 'netherite_chestplate';
 
 
+-- Attempt at writing player vs player query
+--  diamond helmet + iron chestplate + iron leggings + diamond boots
+SELECT helmet.`Defense Points`, chestplate.`Defense Points`, leggings.`Defense Points`, boots.`Defense Points`
+FROM `Armor` helmet, `Armor` chestplate, `Armor` leggings, `Armor` boots
+WHERE helmet.ARMOR_ID = 
+
+
+
 DESCRIBE `ARMOR`;
 DESCRIBE `Weapon`;
 DESCRIBE `MATERIALS`;
+
+
+
+
+
+-- How much damage does item x deal per attack
+SELECT `Name`, `base damage` AS `Damage per Attack`
+FROM `WEAPON`
+ORDER BY `base damage` DESC
+;
+
+
+-- Shows all diamond ARMOR
+SELECT *
+FROM `ARMOR`
+WHERE `Material Type` = 'diamond';
+
+-- What is the attack speed of item x?
+SELECT `Name`, `reload time`
+FROM `WEAPON`
+-- WHERE `Name` LIKE X
+ORDER BY `reload time` DESC
+;
+
+-- What is the damage per second ( DPS ) of item x?
+SELECT `Name`, `reload time` * `base damage` AS DPS
+FROM `WEAPON`
+-- WHERE `Name` LIKE X
+ORDER BY `reload time` * `base damage` DESC
+;
+
+-- How much total damage item x can deal?
+SELECT `Name`, `durability` * `base damage` AS `Total Damage`
+FROM `WEAPON`
+-- WHERE `Name` LIKE X
+ORDER BY `durability` * `base damage` DESC
+;
+-- How fast can you break item X?
+SELECT `Name`, `durability` / `reload time` AS `Total Damage`
+FROM `WEAPON`
+-- WHERE `Name` LIKE X
+ORDER BY `durability` / `reload time` DESC
+;
+-- Sort all weapons by their reload time
+SELECT `Name`, `reload time`
+FROM `WEAPON`
+-- WHERE `Name` LIKE X
+ORDER BY `reload time` DESC
+;
+
+-- Sort all weapons by the size of its crafting recipe
+-- (count how many 0's are in the recipe)
